509f42dd0d8273d7e701fe24fdcb904a7d1f6224
Fanele Gina <faneleeric@gmail.com>
2025-06-29 01:19:36 +0200

    Fix JSON asset path for deployment

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2494248921"
+    serviceWorkerVersion: "472132986"
   }
 });
